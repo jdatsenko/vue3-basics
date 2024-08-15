3a3cda69c9328cfd9bbe5ad7e128262523811127
Yuliia Datsenko <xdatsenko@stuba.sk>
2024-08-15 12:43:44 +0200

    posts list & form

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="app">
+      <post-form/>
+      <post-list :posts="posts"/>
+    </div>
+</template>
+
+
+<script>
+import PostForm from './components/PostForm.vue'
+import PostList from './components/PostList.vue'
+
+export default {
+    components: {
+        PostForm,
+        PostList
+    },
+    data() {
+        return {
+          posts: [
+            {
+              id: 1,
+              title: 'Post 1',
+              body: 'Post 1 body'
+            },
+            {
+              id: 2,
+              title: 'Post 2',
+              body: 'Post 2 body'
+            },
+            {
+              id: 3,
+              title: 'Post 3',
+              body: 'Post 3 body'
+            }
+          ]
+        }
+    },
+    methods: {
+       
+    }
+}
+</script>
+
+<style></style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,66 @@
+<template>
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input
+        v-focus
+        v-model="post.title"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-model="post.body"
+        type="text"
+        placeholder="Описание"
+      />
+      <button
+      class="btn"
+        @click="createPost"
+      >
+        Создать
+      </button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        post: {
+          title: '',
+          body: ''
+        }
+      }
+    },
+    methods: {
+      createPost() {
+        this.post.id = Date.now();
+        this.$emit('create', this.post)
+        this.post = {
+          title: '',
+          body: ''
+        }
+      }
+    },
+  }
+  </script>
+  
+  <style scoped>
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    margin: 0 auto;
+  }
+  input {
+    margin-bottom: 10px;
+    padding: 5px;
+  }
+  .btn {
+    padding: 5px;
+    background-color: teal;
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
+  
+  </style>
