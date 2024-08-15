becd5b04c4fde699cbf5e2febd319030d67e1f76
Yuliia Datsenko <xdatsenko@stuba.sk>
2024-08-15 19:10:02 +0200

    custom select

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,89 @@
 <template>
   <div class="app">
     <h1>Page for posts</h1>
-
-    <my-button @click="dialogVisible = true">Create post</my-button>
+    <div class="app__btns">
+      <my-button @click="dialogVisible = true">Create post</my-button>
+      <my-select
+      v-model="selectedSort"
+      :options="sortOptions"
+      />
+    </div>
 
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
     </my-dialog>
 
-    <post-list :posts="posts" @remove="removePost" v-if="!isPostLoading" />
+    <post-list :posts="sortedPosts" @remove="removePost" v-if="!isPostLoading" />
     <div v-else>Loading...</div>
   </div>
 </template>
 
-
 <script>
-import PostForm from './components/PostForm.vue'
-import PostList from './components/PostList.vue'
-import axios from 'axios'
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
+import axios from "axios";
 
 export default {
   components: {
     PostForm,
-    PostList
+    PostList,
   },
   data() {
     return {
       posts: [],
       dialogVisible: false,
-      isPostLoading: false
-    }
+      isPostLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        { name: "Name", value: "title" },
+        { name: "Description", value: "body" },
+      ],
+    };
   },
   methods: {
     createPost(post) {
-      this.posts.push(post)
-      this.dialogVisible = false
+      this.posts.push(post);
+      this.dialogVisible = false;
     },
 
     removePost(post) {
-      this.posts = this.posts.filter(p => p.id !== post.id)
+      this.posts = this.posts.filter((p) => p.id !== post.id);
     },
 
     async fetchPosts() {
       try {
-        this.isPostLoading = true
-          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
-          this.posts = response.data
-          this.isPostLoading = false
+        this.isPostLoading = true;
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts?_limit=5"
+        );
+        this.posts = response.data;
+        this.isPostLoading = false;
+      } catch (e) {
+        console.log(e);
       }
-      catch (e) {
-        console.log(e)
-      }
-
-    }
+    },
   },
 
   mounted() {
-    this.fetchPosts()
-  }
-}
+    this.fetchPosts();
+  },
+
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => {
+        return post1[this.selectedSort].localeCompare(post2[this.selectedSort]);
+      });
+    },
+  },
+
+  watch: {
+  },
+};
 </script>
 
-<style></style>
+<style>
+.app__btns {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,8 +1,10 @@
 import MyButton from './MyButton.vue'
 import MyInput from './MyInput.vue'
 import MyDialog from './MyDialog.vue'
+import MySelect from './MySelect.vue'
 export default [
     MyButton,
     MyInput,
-    MyDialog
+    MyDialog,
+    MySelect
 ]
