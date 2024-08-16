0e5fb5738ddf90ec13c22c55c2c806431600c04f
Yuliia Datsenko <xdatsenko@stuba.sk>
2024-08-16 02:16:33 +0200

    toggle mixin

--- src/components/UI/MyDialog.vue
+++ src/components/UI/MyDialog.vue
@@ -1,48 +1,36 @@
 <template>
-    <div class="dialog" v-if="show" @click="hideDialog">
-        <div @click.stop class="dialog__content">
-            <slot></slot>
-        </div>
+  <div class="dialog" v-if="show" @click="hideDialog">
+    <div @click.stop class="dialog__content">
+      <slot></slot>
     </div>
+  </div>
 </template>
 
 <script>
-
+import toggleMixin from "@/mixins/toggleMixin";
 export default {
-    name: 'my-dialog',
-
-    props: {
-        show: {
-            type: Boolean,
-            default: false
-        }
-    },
-
-    methods: {
-        hideDialog() {
-            this.$emit('update:show', false)
-        }
-    }
-}
+  name: "my-dialog",
+  mixins: [toggleMixin],
+};
 </script>
 
 <style scoped>
 .dialog {
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    background: rgba(0, 0, 0, 0.5);
-    position: fixed;
-    display: flex;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
 }
 
 .dialog__content {
-    margin: auto;
-    background: white;
-    border-radius: 12px;
-    min-height: 50px;
-    min-width: 500px;
-    padding: 20px;
+  margin: auto;
+  background: white;
+  border-radius: 12px;
+  min-height: 50px;
+  min-width: 500px;
+  padding: 20px;
 }
 </style>
